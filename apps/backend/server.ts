b89d5989d95f338a8930f68f295cff36698db221
Dirk de Visser <github@dirkdevisser.nl>
2024-02-15 19:39:02 +0100

    feat(backend): add server.ts and handle process signals correctly
    Signed-off-by: Dirk de Visser <github@dirkdevisser.nl>

---
+++ apps/backend/server.ts
@@ -0,0 +1,32 @@
+import closeWithGrace from "close-with-grace";
+import { buildApp } from "./app.js";
+
+const app = await buildApp();
+
+const closeGracefully = closeWithGrace({ delay: 500 }, async (options) => {
+	if (options.err) {
+		app.log.error(options.err);
+	}
+
+	await app.close();
+});
+
+app.addHook("onClose", (instance, done) => {
+	closeGracefully.uninstall();
+	return done();
+});
+
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+
+app.listen(
+	{
+		// TODO: we probly need to configure host as well for production.
+		port,
+	},
+	(err) => {
+		if (err) {
+			app.log.error(err);
+			process.exit(1);
+		}
+	},
+);
