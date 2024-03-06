7360e83d2685cbfb680d7d05b32ea08de1305118
Dirk de Visser <github@dirkdevisser.nl>
2024-03-06 16:16:34 +0100

    chore: lint after rebase
    Signed-off-by: Dirk de Visser <github@dirkdevisser.nl>

--- packages/eslint-config/src/markdown.ts
+++ packages/eslint-config/src/markdown.ts
@@ -5,11 +5,11 @@ import markdown from "eslint-plugin-markdown";
  * Allows parsing of markdown files, adding code blocks as virtual files.
  */
 export function markdownConfig(): Linter.FlatConfig[] {
-  return [
-    {
-      plugins: {
-        markdown,
-      },
-    },
-  ];
+	return [
+		{
+			plugins: {
+				markdown,
+			},
+		},
+	];
 }
