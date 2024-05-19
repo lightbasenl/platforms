518853e6b1e3f32ea792df1a2c60454f78c47c54
Dirk de Visser <github@dirkdevisser.nl>
2024-05-19 12:07:46 +0200

    chore(apps/backend): remove sample app
    Signed-off-by: Dirk de Visser <github@dirkdevisser.nl>

--- apps/backend/types.ts
+++
@@ -1,8 +0,0 @@
-import type http from "node:http";
-import type { FastifyInstance } from "fastify";
-
-export type FastifyBase = FastifyInstance<
-	http.Server,
-	http.IncomingMessage,
-	http.ServerResponse
->;
