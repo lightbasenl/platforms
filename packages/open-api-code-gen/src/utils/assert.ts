ff89accd7bd19b60c484de4a41157f98300f09ff
Dirk de Visser <github@dirkdevisser.nl>
2024-11-04 08:54:33 +0100

    chore(open-api-code-gen): convert Brand type to use a Record
    Necessary for the next TypeScript ESLint bump.
    
    See #292 & [action output](https://github.com/lightbasenl/platforms/actions/runs/11624346360/job/32372799028?pr=292)
    
    Signed-off-by: Dirk de Visser <github@dirkdevisser.nl>

--- packages/open-api-code-gen/src/utils/assert.ts
+++ packages/open-api-code-gen/src/utils/assert.ts
@@ -1,6 +1,31 @@
-export type Brand<Type, Brand extends string> = Type & {
-	readonly [B in Brand as `__brand_${Brand}`]: never;
-};
+/**
+ * Brand types, so an unvalidated primitive is not allowed.
+ *
+ * @example
+ * ```
+ * type Age = Brand<number, "age">;
+ *
+ * export function makeAge(value: number): Age {
+ *   if (value >= 0 && value <= 120) {
+ *     return value as Age;
+ *   }
+ *
+ *   throw new Error("Invalid value for 'age'");
+ * }
+ *
+ * function isOlderThan21(age: Age) {
+ *   return age > 21;
+ * }
+ *
+ * // => no type errors
+ * isOlderThan21(makeAge(22));
+ *
+ * // => type error number is not assignable to Age
+ * isOlderThan21(18);
+ * ```
+ */
+export type Brand<Type, Brand extends string> = Type &
+	Readonly<Record<`__brand_${Brand}`, never>>;
 
 export type MaybePromise<Type> = Type | Promise<Type>;
 
