b3c9c64203cab32b885241136392a5477e9fe3f7
Dirk de Visser <github@dirkdevisser.nl>
2026-02-19 19:22:02 +0100

    chore: drop compas-convert
    Signed-off-by: Dirk de Visser <github@dirkdevisser.nl>

--- packages/compas-convert/src/pass.ts
+++
@@ -1,6 +0,0 @@
-import type { SourceFile } from "ts-morph";
-import type { Context } from "./context.js";
-
-export type GlobalPass = (context: Context) => void | Promise<void>;
-export type FilePass = (context: Context, file: SourceFile) => void | Promise<void>;
-export type Pass = GlobalPass | FilePass;
--- packages/compas-convert/src/passes/fix-missing-imports.ts
+++
@@ -1,9 +0,0 @@
-import type { SourceFile } from "ts-morph";
-import type { Context } from "../context.js";
-
-/**
- * Automatically try to add missing imports via the TS language server.
- */
-export function fixMissingImports(context: Context, sourceFile: SourceFile) {
-	sourceFile.fixMissingImports();
-}
